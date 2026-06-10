4f4061e95c02a24b878f4cb05af8b6faef55d89e
Siddhartha2507 <siddharthadaram@gmail.com>
2026-06-10 22:50:26 +0530

    Add comparison API endpoint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/compare", methods=["POST"])
+def compare():
+    data = request.json
+    return jsonify({
+        "message": "Compare API working",
+        "received": data
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
